de42c7c38b17f70b60f3eceef8754be94cb785fd
MogamiTsuchikawa <atsuyatsuchikawa@gmail.com>
2020-03-02 03:27:43 +0900

    残りエラー26

--- bookers2/app/controllers/application_controller.rb
+++ bookers2/app/controllers/application_controller.rb
@@ -7,4 +7,18 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.permit(:sign_up, keys: [:introduction])
     devise_parameter_sanitizer.permit(:sign_up, keys: [:profile_image_id])
   end
+  def after_sign_in_path_for(resource)
+    if current_user
+      flash[:notice] = "ログインに成功しました" 
+      user_path(current_user.id)  #　指定したいパスに変更
+    
+    end
+  end
+  def after_sign_up_path_for(resource)
+    if current_user
+      flash[:notice] = "ログインに成功しました" 
+      user_path(current_user.id)  #　指定したいパスに変更
+    
+    end
+  end
 end
--- bookers2/app/controllers/books_controller.rb
+++ bookers2/app/controllers/books_controller.rb
@@ -17,7 +17,23 @@ class BooksController < ApplicationController
         @books = Book.all
     end
     def show
+        @book = Book.new
+        @target = Book.find(params[:id])
+        @user = User.find(current_user.id)
+    end
+    def edit
         @book = Book.find(params[:id])
+        
+    end
+    def update
+        book = Book.find(params[:id])
+        book.update(book_params)
+        redirect_to book_path
+    end
+    def destroy
+        book = Book.find(params[:id])
+        book.destroy
+        redirect_to "/books"
     end
     private
     def book_params
